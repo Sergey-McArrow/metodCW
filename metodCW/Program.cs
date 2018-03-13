3c273e090ba93f8cf259bac8312e83fb657b72ff
Andrey Salnikov <a.salnikov.home@gmail.com>
2018-03-13 20:23:04 +0200

    metCW

--- metodCW/Program.cs
+++ metodCW/Program.cs
@@ -8,8 +8,35 @@ namespace metodCW
 {
     class Program
     {
+        static int Factorial (int n)
+        {
+            if (n == 0)
+            return 1;
+            return n * Factorial (n-1);
+        }
+
+        static void IsItDividedOfTwo(double n)
+        {
+
+            if (n == 1)
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+            else if (n<1)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            IsItDividedOfTwo(n/2);
+
+        }
         static void Main(string[] args)
         {
+            //Console.WriteLine(Factorial(5));
+            //IsItDividedOfTwo(4);
+
+
         }
     }
 }
